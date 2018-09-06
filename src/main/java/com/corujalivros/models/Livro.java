7611fee9af0902ef3c15ab27d6cfb5040b49d4f2
Leonardo de Souza Faria <leonardo.fari@hotmail.com>
2018-09-05 21:36:21 -0300

    Criação do model e classe livro

---
+++ src/main/java/com/corujalivros/models/Livro.java
@@ -0,0 +1,46 @@
+package com.corujalivros.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+public class Livro {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int codigo;
+	
+	@NotBlank
+	private String nome;
+	
+	@NotBlank
+	private int quantidadePaginas;
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getQuantidadePaginas() {
+		return quantidadePaginas;
+	}
+
+	public void setQuantidadePaginas(int quantidadePaginas) {
+		this.quantidadePaginas = quantidadePaginas;
+	}
+	
+}
