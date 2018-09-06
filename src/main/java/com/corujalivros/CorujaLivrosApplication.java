cde279bca20128efd2b9ba5094b507acb2b10aac
Leonardo de Souza Faria <leonardo.fari@hotmail.com>
2018-09-05 21:15:26 -0300

    Projeto inicial

---
+++ src/main/java/com/corujalivros/CorujaLivrosApplication.java
@@ -0,0 +1,12 @@
+package com.corujalivros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorujaLivrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorujaLivrosApplication.class, args);
+	}
+}
---
+++ src/test/java/com/corujalivros/corujalivros/CorujaLivrosApplicationTests.java
@@ -0,0 +1,16 @@
+package com.corujalivros.corujalivros;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CorujaLivrosApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
